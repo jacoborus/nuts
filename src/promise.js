c17fb363a8bb6a3592419ba9bf6fd68c58861a37
Jacobo Tabernero <jacobo@xarope.eu>
2015-02-02 18:38:47 -0600

    clean promise.js

--- src/promise.js
+++ src/promise.js
@@ -1,15 +1,5 @@
 'use strict';
 
-var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-var ARGUMENT_NAMES = /([^\s,]+)/g;
-function getParamNames(func) {
-  var fnStr = func.toString().replace(STRIP_COMMENTS, '');
-  var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-  if(result === null)
-     result = [];
-  return result;
-}
-
 var Prom = function () {
 	this.fns = [];
 
