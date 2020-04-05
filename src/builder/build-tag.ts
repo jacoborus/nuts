be73a77bf67ff03c0995207f5f4a7274996aadb8
Jacobo Tabernero <jacoborus@gmail.com>
2020-04-05 02:08:29 +0200

    Update: BuildChildren in its own file

--- src/builder/build-tag.ts
+++ src/builder/build-tag.ts
@@ -1,34 +1,13 @@
-import { buildText } from './build-text'
 import { buildAttribs } from './build-attribs'
-import { buildNut } from './build-nut'
-import { buildConditional } from './build-conditional'
+import { buildChildren } from './build-children'
 
 import {
-  TagSchema,
-  ElemSchema,
-  NutType,
-  ElemBuilder
+  TagSchema
 } from '../common'
-
-type Builders = {[ K in NutType ]: ElemBuilder}
-const builders: Builders = {
-  tag: buildTag as ElemBuilder,
-  text: buildText as ElemBuilder,
-  nut: buildNut as ElemBuilder,
-  conditional: buildConditional as ElemBuilder
-}
 
 export function buildTag (schema: TagSchema): string {
   const { name, attribs, children } = schema
   const atts = buildAttribs(attribs)
   const childTags = buildChildren(children)
   return `renderTag('${name}',[${atts}],[${childTags}])`
-}
-
-function buildChildren (children: ElemSchema[]): string {
-  const childTags = children.map(child => {
-    const build = builders[child.kind]
-    return build(child)
-  })
-  return childTags.join(',')
 }
