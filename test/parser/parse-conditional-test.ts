aae84fc0f70749585cb4b0cf1f69d4231713e5dc
Jacobo Tabernero <jacoborus@gmail.com>
2020-04-11 17:43:12 +0200

    Update: Test conditionalVar tag parsers

--- test/parser/parse-conditional-test.ts
+++ test/parser/parse-conditional-test.ts
@@ -64,7 +64,7 @@ test('Parse conditional: if-else-const', t => {
     }],
     children: []
   }
-  const base = [...baseChildren]
+  const base = baseChildren.slice()
   base.splice(2, 0, elseTag)
   const [first, second, third] = parseConditional(base as ElemSchema[])
   const result = {
@@ -89,5 +89,90 @@ test('Parse conditional: if-else-const', t => {
   t.same(first, baseChildren[0])
   t.same(second, result)
   t.same(third, baseChildren[2])
+  t.end()
+})
+
+test('Parse conditional: if-var', t => {
+  const iftag = {
+    kind: 'tag',
+    name: 'span',
+    attribs: [{
+      kind: 'conditionalVar',
+      propName: '(if)',
+      value: ': x.y',
+      variables: ['x.y']
+    }],
+    children: []
+  }
+  const base = baseChildren.slice()
+  base.splice(1, 1, iftag)
+  const [first, second, third] = parseConditional(base as ElemSchema[])
+  const result = {
+    kind: 'conditionalVar',
+    conditions: ['box => !!box.x?.y'],
+    variables: ['x.y'],
+    children: [
+      {
+        kind: 'tag',
+        name: 'span',
+        attribs: [],
+        children: []
+      }
+    ]
+  }
+  t.same(first, baseChildren[0])
+  t.same(second, result)
+  t.same(third, baseChildren[2])
+  t.end()
+})
+
+test('Parse conditional: if-else-var', t => {
+  const ifTag = {
+    kind: 'tag',
+    name: 'span',
+    attribs: [{
+      kind: 'conditionalVar',
+      propName: '(if)',
+      value: ': x.y',
+      variables: ['x.y']
+    }],
+    children: []
+  }
+  const elseTag = {
+    kind: 'tag',
+    name: 'div',
+    attribs: [{
+      kind: 'conditionalConst',
+      propName: '(else)',
+      value: '',
+      variables: []
+    }],
+    children: []
+  }
+  const base = baseChildren.slice()
+  base.splice(1, 1, ifTag, elseTag)
+  const [first, second, third] = parseConditional(base as ElemSchema[])
+  const result = {
+    kind: 'conditionalVar',
+    conditions: ['box => !!box.x?.y'],
+    variables: ['x.y'],
+    children: [
+      {
+        kind: 'tag',
+        name: 'span',
+        attribs: [],
+        children: []
+      },
+      {
+        kind: 'tag',
+        name: 'div',
+        attribs: [],
+        children: []
+      }
+    ]
+  }
+  t.same(first, baseChildren[0], 'pre')
+  t.same(second, result, 'main')
+  t.same(third, baseChildren[2], 'post')
   t.end()
 })
