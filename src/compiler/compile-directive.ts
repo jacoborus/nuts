930aa2f82086f4a3f1897a965d337d3f14ddd6c7
Jacobo Tabernero Rey <jacoborus@gmail.com>
2022-02-20 00:36:55 +0100

    Update: compileDirective loopSchema and TreeSchema

--- src/compiler/compile-directive.ts
+++ src/compiler/compile-directive.ts
@@ -1,11 +1,10 @@
-import { LoopSchema, DirectiveSchema, TreeSchema } from '../types';
+import { LoopSchema, TreeSchema } from '../types';
 import { compileChildren } from './compile-tag';
 import { compileExpression } from './compile-expression';
 
-export function compileDirective(directive: DirectiveSchema): string {
-  // if (directive.type === 'loop') return compileLoop(directive);
-  return compileLoop(directive as LoopSchema);
-  // return compileConditional(directive as CondSchema);
+export function compileDirective(directive: LoopSchema | TreeSchema): string {
+  if (directive.type === 'loop') return compileLoop(directive);
+  return compileTree(directive);
 }
 
 export function compileLoop(directive: LoopSchema): string {
