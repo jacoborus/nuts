ab6ffc422287475bd61a7bc860c5b7a6b63d0b5d
Jacobo Tabernero Rey <jacoborus@gmail.com>
2022-03-15 00:03:23 +0100

    Update: add getVoidAttribute

--- src/parser/util.ts
+++ src/parser/util.ts
@@ -12,26 +12,27 @@ export function extractLoopAtts(atts: AttSchema[]): LoopAtts {
   const index = indexAtt ? indexAtt.value : undefined;
   const posAtt = atts.find((att) => att.isDirective && att.name === 'pos');
   const pos = posAtt ? posAtt.value : undefined;
-  const pretarget = atts
-    .find(
-      ({ name, value }) =>
-        name.startsWith('(') && name.endsWith(')') && value === ''
-    )
-    ?.name.slice(1, -1);
+  const voidTarget = getVoidAttribute(atts)?.name.slice(1, -1);
+  const attTarget = atts.find(
+    (att) => att.name === 'target' && att.isDirective
+  )?.value;
+  const pretarget = voidTarget || attTarget;
   const target = pretarget ? parseExpression(pretarget) : [];
   return { pos, index, target };
 }
 
 export function extractTreeRequirement(atts: AttSchema[]): Expression {
-  const voidTarget = atts
-    .find(
-      ({ name, value }) =>
-        name.startsWith('(') && name.endsWith(')') && value === ''
-    )
-    ?.name.slice(1, -1);
+  const voidTarget = getVoidAttribute(atts)?.name.slice(1, -1);
   const pretarget = atts.find(
     (att) => ['if', 'elseif'].includes(att.name) && att.isDirective
   )?.value;
   const target = voidTarget || pretarget;
   return target ? parseExpression(target) : [];
+}
+
+function getVoidAttribute(atts: AttSchema[]): AttSchema | undefined {
+  return atts.find(
+    ({ name, value }) =>
+      name.startsWith('(') && name.endsWith(')') && value === ''
+  );
 }
